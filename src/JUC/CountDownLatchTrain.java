d70a8d75c99b3c2d87a8529054ac2a59cdbcdc1e
Miao <1160292058@qq.com>
2018-05-16 23:59:03 +0800

    CountDownLatch

---
+++ src/JUC/CountDownLatchTrain.java
@@ -0,0 +1,46 @@
+package JUC;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchTrain {
+
+    public void test1() {
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        for (int i = 0; i < 3; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " wait;");
+                try {
+                    latch.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " start");
+            }).start();
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        latch.countDown();
+    }
+
+    public void test2() {
+        final CountDownLatch latch = new CountDownLatch(3);
+
+        for (int i = 0; i < 3; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " start");
+                latch.countDown();
+            }).start();
+        }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("All end");
+    }
+}
